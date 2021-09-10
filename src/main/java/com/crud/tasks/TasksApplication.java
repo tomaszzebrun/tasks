81be87c03b74728868d09396a534bb4e3c397dc2
Tomasz Zebrun <tomasz.zebrun@tron.net.pl>
2021-09-10 08:53:26 +0200

    New repository

---
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -0,0 +1,26 @@
+package com.crud.tasks;
+
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksApplication {
+
+	public static void main(String[] args) {
+
+/*		TaskDto taskDto = new TaskDto(
+				(long)1,
+				"Test title",
+				"Test content"
+		);
+		Long id = taskDto.getId();
+		String title = taskDto.getTitle();
+		String content = taskDto.getContent();
+
+		System.out.println(id + " " + title + " " + content);*/
+
+		SpringApplication.run(TasksApplication.class, args);
+	}
+
+}
