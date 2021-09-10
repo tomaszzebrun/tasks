bdd0d3c0d0583734970e5d235bc6e0afd97d9def
Tomasz Zebrun <tomasz.zebrun@tron.net.pl>
2021-09-10 09:15:00 +0200

    18.3 TasksApplication

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -8,18 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TasksApplication {
 
 	public static void main(String[] args) {
-
-/*		TaskDto taskDto = new TaskDto(
-				(long)1,
-				"Test title",
-				"Test content"
-		);
-		Long id = taskDto.getId();
-		String title = taskDto.getTitle();
-		String content = taskDto.getContent();
-
-		System.out.println(id + " " + title + " " + content);*/
-
 		SpringApplication.run(TasksApplication.class, args);
 	}
 
