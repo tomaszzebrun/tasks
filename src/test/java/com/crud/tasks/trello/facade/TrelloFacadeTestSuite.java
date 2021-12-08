db47b13ae048b76c92309d00bc38dc5538a9e9d8
Tomasz Zebrun <tomasz.zebrun@tron.net.pl>
2021-12-08 06:48:11 +0100

    29.2 TrelloFacade tests

--- src/test/java/com/crud/tasks/trello/facade/TrelloFacadeTestSuite.java
+++ src/test/java/com/crud/tasks/trello/facade/TrelloFacadeTestSuite.java
@@ -3,25 +3,12 @@ package com.crud.tasks.trello.facade;
 import com.crud.tasks.domain.CreatedTrelloCardDto;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
-import com.crud.tasks.service.TrelloService;
-import com.crud.tasks.trello.client.TrelloClient;
-import com.crud.tasks.trello.config.TrelloConfig;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.client.RestTemplate;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class TrelloFacadeTestSuite {
